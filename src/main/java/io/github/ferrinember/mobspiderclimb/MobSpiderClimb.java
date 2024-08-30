a043e5e9d49d5aefbffb9c2208802e1d19115dfc
Ferrin <luke.acuff@gmail.com>
2024-08-29 21:49:07 -0400

    Cleaned up configuration options

---
+++ src/main/java/io/github/ferrinember/mobspiderclimb/MobSpiderClimb.java
@@ -0,0 +1,31 @@
+package io.github.ferrinember.mobspiderclimb;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(MobSpiderClimb.MOD_ID)
+public class MobSpiderClimb
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "mobspiderclimb";
+    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+    public MobSpiderClimb()
+    {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+}
